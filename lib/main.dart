a08966d01beef2d7307aec764995cffaad278fc7
Md. Al amin <alamincsecu@gmail.com>
2022-03-25 09:52:20 +0600

    Outputing widget conditionally and setting up gradient text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 //created widget
 import './question.dart';
@@ -19,76 +18,124 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  var _numOfQuestions;
+  // ignore: prefer_typing_uninitialized_variables
+  //var _numOfQuestions;
+  var questions = const [
+    {
+      'questionText': 'What\'s your favorite color ?',
+      'answers': ['Red', 'Green', 'Blue', 'White', 'Pink', 'Ash']
+    },
+    {
+      'questionText': 'What\'s your favorite pet animal?',
+      'answers': ['Cat', 'Dog', 'Cow', 'Goat']
+    },
+    {
+      'questionText': 'What\'s your favorite game ?',
+      'answers': [
+        'Cricket',
+        'Football',
+        'Ha-Du-Du',
+        'Kabadi',
+        'Tenis',
+        'Badminton'
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite book?',
+      'answers': [
+        'The Al-Quran',
+        'The Alchemist',
+        'Theory of Everything',
+        'Evolution of History'
+      ]
+    },
+  ];
+  //final _numberOfQuestions = questions.length;
   void _answerQuestion() {
     //print(questionIndex);
     setState(() {
       _questionIndex = _questionIndex + 1;
-      if (_questionIndex >= _numOfQuestions) {
-        //Text("Wow !!! You Made it.");
-        _questionIndex = 0;
-      }
+      // if (_questionIndex >= questions.length) {
+      //   //Text("Wow !!! You Made it.");
+      //   _questionIndex = 0;
+      // }
     });
-    print(_numOfQuestions);
+    //print(_numOfQuestions);
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color ?',
-        'answers': ['Red', 'Green', 'Blue', 'White', 'Pink', 'Ash']
-      },
-      {
-        'questionText': 'What\'s your favorite pet animal?',
-        'answers': ['Cat', 'Dog', 'Cow', 'Goat']
-      },
-      {
-        'questionText': 'What\'s your favorite game ?',
-        'answers': [
-          'Cricket',
-          'Football',
-          'Ha-Du-Du',
-          'Kabadi',
-          'Tenis',
-          'Badminton'
-        ]
-      },
-      {
-        'questionText': 'What\'s your favorite book?',
-        'answers': [
-          'The Al-Quran',
-          'The Alchemist',
-          'Theory of Everything',
-          'Evolution of History'
-        ]
-      },
-    ];
-    _numOfQuestions = questions.length;
     return MaterialApp(
       title: 'QuizApp',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Personality Calculator'),
-          ),
-          body: Column(
-            children: [
-              Question(
-                questions[_questionIndex]['questionText'] as String,
-              ),
+        appBar: AppBar(
+          title: const Text('Personality Calculator'),
+        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                  ),
 
-              ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-              // Answer(_answerQuestion),
-              // Answer(_answerQuestion),
-              // Answer(_answerQuestion),
-            ],
-          )),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                  // Answer(_answerQuestion),
+                  // Answer(_answerQuestion),
+                  // Answer(_answerQuestion),
+                ],
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Text(
+                    //   'Congratulations!!! You\'ve done it.',
+                    //   style: TextStyle(
+                    //     fontSize: 20,
+                    //     fontWeight: FontWeight.w400,
+                    //     color: Colors.teal,
+                    //   ),
+                    // ),
+                    GradientText(
+                      'Congratulations!!! You\'ve done it.',
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      colors: const [
+                        Colors.blue,
+                        Colors.red,
+                        Colors.teal,
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _questionIndex = 0;
+                        });
+                      },
+                      child: GradientText(
+                        'Restart',
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        colors: const [
+                          Colors.white,
+                          Colors.yellow,
+                          Colors.white,
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+      ),
     );
   }
 }
